6c87bf093ef6f6d8ef57e4033f128996907809b9
almo <arumoasu@gmail.com>
2025-12-27 21:58:41 +0900

    Add TypeScript interfaces for navigation and content

---
+++ types.ts
@@ -0,0 +1,24 @@
+
+export interface NavItem {
+  label: string;
+  href: string;
+}
+
+export interface CastClass {
+  name: string;
+  price: string;
+  description: string;
+  color: string;
+}
+
+export interface Review {
+  name: string;
+  age: string;
+  content: string;
+  rating: number;
+}
+
+export interface FAQItem {
+  question: string;
+  answer: string;
+}
